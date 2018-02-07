b37c7ec1bd58dd8936f72fac3db54c1f2225be38
Freddy Montes <freddymontes@gmail.com>
2018-02-06 20:34:51 -0600

    Animate button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,15 @@
 const actionButton = document.querySelector('.actions .btn');
+const width = actionButton.offsetWidth;
+actionButton.style.width = `${width}px`;
 
 actionButton.addEventListener('click', (event) => {
-    event.currentTarget.classList.toggle('icon');
-})
+    const el = event.currentTarget;
+
+    el.classList.toggle('icon');
+
+    if (!el.classList.contains('icon')) {
+        el.style.width = `${width}px`;
+    } else {
+        el.removeAttribute('style');
+    }
+});
