ba0dc36312d67d9b14fe0b4d20a117b3e20eae8d
Freddy Montes <freddymontes@gmail.com>
2018-02-18 12:28:28 -0600

    Everything in place

---
+++ app/scripts/data.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        barcode: '123',
+        quantity: 1,
+        name: 'Candy Bar',
+        price: 120
+    },
+    {
+        barcode: '456',
+        quantity: 1,
+        name: 'Coca Cola 600ml',
+        price: 90
+    },
+    {
+        barcode: '789',
+        quantity: 1,
+        name: 'Water bottle 250ml',
+        price: 60
+    },
+    {
+        barcode: '321',
+        quantity: 1,
+        name: 'Cake',
+        price: 12000
+    }
+];
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,80 @@
-let pageState = 'home';
+Number.prototype.toCurrency = function() {
+    return `$${this.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}`;
+};
 
-const wrapper = document.querySelector('.wrapper');
+function addProduct(product) {
+    const existingProduct = getExistingProduct(product.barcode);
 
-const actionsPaymentButton = document.querySelector('.actions__payment');
-const actionsPaymentButtonWidth = actionsPaymentButton.offsetWidth;
-actionsPaymentButton.style.width = `${actionsPaymentButtonWidth}px`;
+    if (existingProduct) {
+        updateProductInList(existingProduct, product);
+    } else {
+        productList.appendChild(getProductListItem(product));
+    }
 
-actionsPaymentButton.addEventListener('click', event => {
-    setState(pageState === 'home' ? 'payment' : 'done');
-});
+    updateProductsModel(product);
+    setInvoice();
+}
 
-const paymentMethodSelector = document.querySelector('.actions__payment-method');
+function getExistingProduct(barcode) {
+    return document.querySelector(`li[data-barcode="${barcode}"]`);
+}
 
-paymentMethodSelector.addEventListener('click', (event) => {
-    document.querySelector('.payment-method--selected').classList.remove('payment-method--selected');
-    event.target.classList.toggle('payment-method--selected')
-});
+function getPriceModel() {
+    return productsModel.map(product => product.price);
+}
 
-const backButton = document.querySelector('.actions__back');
-backButton.addEventListener('click', ($event) => {
-    setState('home');
-});
+function getProductListItem(data) {
+    const fragment = document.createDocumentFragment();
+    const li = document.createElement('li');
+    li.dataset.barcode = data.barcode;
+    li.classList.add('products__item');
+    li.innerHTML = `
+        <span class="products__quantity">${data.quantity}</span>
+        <span class="products__name">${data.name}</span>
+        <span class="products__price">${data.price.toCurrency()}</span>
+    `;
+    fragment.appendChild(li);
+    return fragment;
+}
 
+function getRandomProduct() {
+    return data[Math.floor(Math.random() * Math.floor(data.length))];
+}
 
-const setState = (state) => {
+function getSubtotal() {
+    return productsModel.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.price * currentValue.quantity;
+    }, 0);
+}
+
+function getTax() {
+    return getSubtotal() * 0.13;
+}
+
+function getTotal() {
+    return getSubtotal() + getTax();
+}
+
+function resetApp() {
+    document.querySelector('.cash__paying-field').value = '';
+    document.querySelector('.cash__change-ammout').innerHTML = '$0.00';
+    productList.innerHTML = '';
+    subtotalElem.innerHTML = '';
+    taxElem.innerHTML = '';
+    totalElem.innerHTML = '';
+    actionsPaymentButton.setAttribute('disabled', true);
+    productsModel = [];
+}
+
+function setInvoice(priceProduct) {
+    const pricesModel = getPriceModel();
+    subtotalElem.innerHTML = getSubtotal().toCurrency();
+    taxElem.innerHTML = getTax().toCurrency();
+    totalElem.innerHTML = getTotal().toCurrency();
+    document.querySelector('.card__ammout').innerHTML = totalElem.innerHTML;
+}
+
+function setState(state) {
     if (state === 'payment') {
         pageState = 'payment';
         wrapper.classList.add('payment');
@@ -31,17 +82,82 @@ const setState = (state) => {
     } else if (state === 'home') {
         pageState = 'home';
         wrapper.classList.remove('payment');
-        actionsPaymentButton.style.width = `${actionsPaymentButtonWidth}px`;   
+        actionsPaymentButton.style.width = `${actionsPaymentButtonWidth}px`;
     } else if (state === 'done') {
         wrapper.classList.add('done');
         pageState = 'home';
 
         setTimeout(() => {
             wrapper.classList.remove('payment');
-        }, 150)
-        
+            resetApp();
+        }, 150);
+
         setTimeout(() => {
             wrapper.classList.remove('done');
-        }, 800)
+        }, 800);
     }
 }
+
+function updateProductsModel(products) {
+    if (productsModel.includes(products)) {
+        productsModel = productsModel.map(product => {
+            if (product.barcode === products.barcode) {
+                product.quantity++;
+            }
+
+            return product;
+        });
+    } else {
+        productsModel.push(products);
+    }
+}
+
+function updateProductInList(productEl, product) {
+    const quantity = parseInt(productEl.querySelector('.products__quantity').innerHTML) + 1;
+    productEl.querySelector('.products__price').innerHTML = (product.price * quantity).toCurrency();
+    productEl.querySelector('.products__quantity').innerHTML = quantity;
+}
+
+const actionsPaymentButton = document.querySelector('.actions__payment');
+const actionsPaymentButtonWidth = actionsPaymentButton.offsetWidth;
+const backButton = document.querySelector('.actions__back');
+const paymentMethodSelector = document.querySelector('.actions__payment-method');
+const productList = document.querySelector('.products');
+const subtotalElem = document.querySelector('.subtotal__ammount');
+const taxElem = document.querySelector('.tax__ammount');
+const totalElem = document.querySelector('.total__ammount');
+const wrapper = document.querySelector('.wrapper');
+let pageState = 'home';
+let productsModel = [];
+
+actionsPaymentButton.style.width = `${actionsPaymentButtonWidth}px`;
+
+actionsPaymentButton.addEventListener('click', event => {
+    setState(pageState === 'home' ? 'payment' : 'done');
+});
+
+paymentMethodSelector.addEventListener('click', event => {
+    if (!event.target.classList.contains('payment-method--selected')) {
+        document.querySelector('.payment-selected').classList.toggle('payment-selected--card');
+        document.querySelector('.payment-method--selected').classList.remove('payment-method--selected');
+        event.target.classList.toggle('payment-method--selected');
+    }
+});
+
+backButton.addEventListener('click', $event => {
+    setState('home');
+});
+
+document.querySelector('.barcode').addEventListener('click', () => {
+    addProduct(getRandomProduct());
+
+    if (productsModel.length) {
+        actionsPaymentButton.removeAttribute('disabled');
+    }
+});
+
+document.querySelector('.cash__paying-field').addEventListener('keyup', event => {
+    document.querySelector('.cash__change-ammout').innerHTML = event.target.value
+        ? (event.target.value - getTotal()).toCurrency()
+        : parseInt('0').toCurrency();
+});
