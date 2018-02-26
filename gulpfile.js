c65e51d94f6a7ae41306bccdc56ae48f159807df
Freddy Montes <freddymontes@gmail.com>
2018-02-25 21:38:47 -0600

    Add gh gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -182,6 +182,11 @@ gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
     return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }));
 });
 
+gulp.task('deploy', ['default'], () => {
+    return gulp.src('dist/**/*')
+      .pipe($.ghPages());
+  });
+
 gulp.task('default', () => {
     return new Promise(resolve => {
         dev = false;
